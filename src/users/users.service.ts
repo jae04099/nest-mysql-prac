fd437c321380e44efab2ce2d69dfbb206c15aa74
gyurilee <jae04099@gmail.com>
2022-04-22 18:32:18 +0900

    ㅇㄹㄴㄴ

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/users.entity';
-import { getConnection } from 'typeorm';
 
 @Injectable()
 export class UsersService {
